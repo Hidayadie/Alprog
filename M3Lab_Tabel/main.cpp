aa031293c3fc5356bf6f6711e7c5e5448dcc4ff2
Hidayadie <hissats234@gmail.com>
2023-09-20 20:22:32 +0700

    kode tabel

--- M3Lab_Tabel/main.cpp
+++ M3Lab_Tabel/main.cpp
@@ -2,7 +2,7 @@
 #include <iomanip>
 
 using namespace std;
-void kasirTes(), kosongan();
+void kasirTes(), cumaTes();
 int main() {
     cout << "+----+-------------------------+------+" <<endl;
     cout << "|" << setw(3)<<"No"<< " |" << setw(24)<<"Item" << " |" << setw(5) << "Qty"<< " |"<<endl;
@@ -17,17 +17,29 @@ int main() {
     cout << "+----+-------------------------+------+" <<endl;
 }
 
-void kasirTes() {
-        cout << "\t+===================================================+" << endl;
-        cout << "\t|                                                   |" << endl;
-        cout << "\t|           Selamat datang di Toko Saya!            |" << endl;
-        cout << "\t|___________________________________________________|" << endl;
-        cout << "\t| Silahkan masukkan pilihan anda pada sesuai dengan |" << endl;
-        cout << "\t|             Item yang anda ingin beli             |" << endl;
-        cout << "\t|                                                   |" << endl;
-        cout << "\t|                                                   |" << endl;
-        cout << "\t|                                                   |" << endl;
-        cout << "\t+===================================================+" << endl;
-        cout << "\t|       'X' Keluar              'G' Kembali         |" << endl;
-        cout << "\t+===================================================+" << endl;
+
+// ABAIKAN KODE DIBAWAH
+
+void cumaTes() {
+
+    cout << "\t+===================================================+" << endl;
+    cout << "\t|                                                   |" << endl;
+    cout << "\t|           Selamat datang di Toko Saya!            |" << endl;
+    cout << "\t|___________________________________________________|" << endl;
+    cout << "\t| Silahkan masukkan pilihan anda pada sesuai dengan |" << endl;
+    cout << "\t|             Item yang anda ingin beli             |" << endl;
+    cout << "\t|___________________________________________________|" << endl;
+    cout << "\t| No |    Nama barang     |  Stok  |      Harga     |" << endl;
+    cout << "\t|----+--------------------+--------+----------------|" << endl;
+    cout << "\t| 01 | Buku cerita        |  004   |   Rp. 20.000   |" <<endl;
+    cout << "\t| 02 | Buku tulis         |  010   |   Rp. 10.000   |" <<endl;
+    cout << "\t| 03 | Pulpen             |  020   |   Rp. 2.500    |" <<endl;
+    cout << "\t| 04 | Pensil             |  020   |   Rp. 2.000    |" <<endl;
+    cout << "\t| 05 | Penghapus          |  015   |   Rp. 1.500    |" <<endl;
+    cout << "\t| 06 | Peraut             |  009   |   Rp. 1.000    |" <<endl;
+    cout << "\t| 07 | Kertas manila      |  050   |   Rp. 15.000   |" <<endl;
+    cout << "\t| 08 | Kertas origami     |  025   |   Rp. 10.000   |" <<endl;
+    cout << "\t+===================================================+" << endl;
+    cout << "\t|       'X' Keluar              'G' Kembali         |" << endl;
+    cout << "\t+===================================================+" << endl;
 }
