13167d9d40ea874dba4ab8a5c3f37651c12c1df5
Hidayadie <hissats234@gmail.com>
2023-09-22 14:09:05 +0700

    tambah readme

---
+++ M3Lab_Tabel/README.md
@@ -0,0 +1,5 @@
+## Tabel dengan plain setw()
+![Screenshot from 2023-09-22 13-53-42](https://github.com/Hidayadie/Alprog/assets/107916585/793a34de-f3a5-488d-a0a9-752deaaea0e6)
+
+## Tabel dengan isi yang diacak menggunakan setw()
+[Screencast from 22-09-23 13:54:35.webm](https://github.com/Hidayadie/Alprog/assets/107916585/de800e5f-98dc-462b-a83d-05e78fa5058d)
--- M3Lab_Tabel/main.cpp
+++ M3Lab_Tabel/main.cpp
@@ -2,7 +2,6 @@
 #include <iomanip>
 
 using namespace std;
-void kasirTes(), cumaTes();
 int main() {
     cout << "+----+-------------------------+------+" <<endl;
     cout << "|" << setw(3)<<"No"<< " |" << setw(24)<<"Item" << " |" << setw(5) << "Qty"<< " |"<<endl;
@@ -16,32 +15,4 @@ int main() {
     cout << "+----+-------------------------+------+" <<endl;
     cout << "|" << setw(3)<<""<< " |" << setw(24)<<"Total" << " |" << setw(5) << "11"<< " |"<<endl;
     cout << "+----+-------------------------+------+" <<endl;
-}
-
-
-// ABAIKAN KODE DIBAWAH
-
-void cumaTes() {
-    char pilihan;
-    cout << "\t+===================================================+" << endl;
-    cout << "\t|                                                   |" << endl;
-    cout << "\t|           Selamat datang di Toko Saya!            |" << endl;
-    cout << "\t|___________________________________________________|" << endl;
-    cout << "\t| Silahkan masukkan pilihan anda pada sesuai dengan |" << endl;
-    cout << "\t|             Item yang anda ingin beli             |" << endl;
-    cout << "\t|___________________________________________________|" << endl;
-    cout << "\t| No |    Nama barang     |  Stok  |      Harga     |" << endl;
-    cout << "\t|----+--------------------+--------+----------------|" << endl;
-    cout << "\t| 01 | Buku cerita        |  004   |   Rp. 20.000   |" << endl;
-    cout << "\t| 02 | Buku tulis         |  010   |   Rp. 10.000   |" << endl;
-    cout << "\t| 03 | Pulpen             |  020   |   Rp. 2.500    |" << endl;
-    cout << "\t| 04 | Pensil             |  020   |   Rp. 2.000    |" << endl;
-    cout << "\t| 05 | Penghapus          |  015   |   Rp. 1.500    |" << endl;
-    cout << "\t| 06 | Peraut             |  009   |   Rp. 1.000    |" << endl;
-    cout << "\t| 07 | Kertas manila      |  050   |   Rp. 15.000   |" << endl;
-    cout << "\t| 08 | Kertas origami     |  025   |   Rp. 10.000   |" << endl;
-    cout << "\t+===================================================+" << endl;
-    cout << "\t|       'X' Keluar              'G' Kembali         |" << endl;
-    cout << "\t+===================================================+" << endl;
-    cout << "\tPilihan anda: "; cin >> pilihan;
 }
