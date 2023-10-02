cde289aeb7ef65cbfe5d5e809e32ff85b6c99e36
Hidayadie <hissats234@gmail.com>
2023-10-02 11:11:17 +0700

    up tugas m5kelas

---
+++ M5Kelas_BilanganPrima/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+int bilangan;
+bool prima;
+cout << "\t\t+===========================+\n"
+     << "\t\t|      Selamat datang       |\n"
+     << "\t\t| Disini anda akan mengecek |\n"
+     << "\t\t| apakah bilangan yang anda |\n"
+     << "\t\t| masukan merupakan prima / |\n"
+     << "\t\t| bukan prima               |\n"
+     << "\t\t+===========================+\n\n"
+     //<< "ketik '1' jika sudah selesai.\n"
+     << "Bilangan: "; cin >> bilangan;
+
+if (bilangan <= 0) {
+    cout << "masukkan bilangan bulat positif diatas 0: ";cin >> bilangan;}
+else if (bilangan == 1) {
+    cout << "1 bukan bilangan prima, bilangan: "; cin >> bilangan;}
+else {
+    for (int i = 2; i <= bilangan / 2; i++) {
+        if (bilangan % i == 0) {
+            prima = false;
+    }else {prima = true;}
+}
+}
+if (prima == true) {
+    cout << "bilangan "<< bilangan<< " adalah bilangan prima";}
+else {
+    cout << "bilangan "<< bilangan<< " bukan bilangan prima\n\n" << bilangan<<" dapat dibagi dengan: \n";
+        for (int i = 2; i <= bilangan / 2; i++) {
+        if (bilangan % i == 0) {
+            cout << i <<" ";}
+        } cout << "dan bilangan itu sendiri ("<<bilangan<<")";
+}
+cout << "\n\n";
+}
