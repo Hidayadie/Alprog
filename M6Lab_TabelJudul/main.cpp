f5a5c8e9d366f6db84b8e527f13ff966ada1e2ed
Hidayadie <hissats234@gmail.com>
2023-10-12 18:28:53 +0700

    initial m6

---
+++ M6Lab_TabelJudul/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main() {
+    cout << "\t\t+=-=-=-=-=-=-=-=-+\n"
+         << "\t\t| Selamat Datang |\n"
+         << "\t\t+=-=-=-=-=-=-=-=-+\n";
+    sleep(1);
+
+
+
+}
