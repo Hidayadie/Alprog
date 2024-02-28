6706cc3723003ee5d81e334a07369cadc1000e0d
Hidayadie <hissats234@gmail.com>
2024-02-28 20:02:32 +0700

    Ngerapiin aja

--- Semester 1/M07UTS_Program_Perpus/Stable/main.cpp
+++ Semester 1/M07UTS_Program_Perpus/Stable/main.cpp
@@ -6,6 +6,7 @@
 #include <iomanip>
 
 using namespace std;
+string user_sekarang = "Tamu";
 class data_perpus {
     public:
         struct Buku {
@@ -43,7 +44,6 @@ class data_perpus {
                 cout << "Gagal membuka file Buku" << endl;
             }
         }
-
         void tampilkan_data() {
             system("clear");
             ifstream file("databuku.txt");
@@ -93,9 +93,8 @@ class data_perpus {
                 cout << "error ngab";
             }
         }
-
         void edit() {
-            system("cls");
+            system("clear");
             string kode, judul, pengarang, jumHalaman;
             int index; char ngedit, save;
             cout << "pilih nomor index buku yang mau di edit\n(cek di data buku)\nindex: ";cin >> index;
@@ -134,29 +133,158 @@ class data_perpus {
                 }
             }
         }
+        void update() {
+            ofstream file("databuku.txt");
+            if (file.is_open()) {
+                for (size_t o = 0; o < buku.size(); o++) {
+                    file << buku[o].kode << ',' << buku[o].judul << ',' << buku[o].pengarang << ',' << buku[o].jumHalaman << ','<<buku[o].tersedia << "\n";
+                }
+                file.close();
+            }
+            else {
+                cout << "error ngab";
+            }
+        }
 };
-class admin {
 
-};
-class member {
+class client {
+    public:
+        struct User {
+            int index;
+            string akses, nama, password, meminjam, judul;
+        };
+        string name,pw;
+        vector<User> user;
 
+        client() {
+            ifstream data("datauser.txt");
+            if (data.is_open()) {
+                int index = 1;
+                string akses, nama, password, meminjam, judul;
+                while (getline(data, akses, ',')) {
+                    getline(data, nama, ',');
+                    getline(data, password, ',');
+                    getline(data, meminjam, ',');
+                    getline(data, judul);
+
+                    User user_iterasi;
+                    user_iterasi.index = index;
+                    user_iterasi.akses = akses;
+                    user_iterasi.nama = nama;
+                    user_iterasi.password = password;
+                    user_iterasi.meminjam = meminjam;
+                    user_iterasi.judul = judul;
+
+                    user.push_back(user_iterasi);
+                    index++;
+                }
+            data.close();
+            }
+
+            else {
+                cout << "Gagal membuka file User" << endl;
+            }
+        }
+        void buat_akun() {
+            string kompw;
+            cout << "Nama: ";
+            getline(cin, name);
+            cout << "Password: ";
+            getline(cin, pw);
+            cout << "Konfirmasi Password: ";
+            getline(cin, kompw);
+            if (pw == kompw) {
+                cout << "user baru: \"" << name <<"\" berhasil dibuat dengan password: \"" << pw <<"\"\n";
+                ofstream file("datauser.txt", ios::app);
+                file << "member,"<< name << ','<<pw<<",false,\n";
+                update();
+            }
+
+        }
+        string login() {
+
+            //system("clear");
+            cout << "Nama: ";
+            getline(cin, name);
+            if (name == "0") {
+                system("clear");
+                buat_akun();
+            }
+
+            else if (name != "0"){
+                for (size_t i = 0; i < user.size(); i++) {
+                    if (user[i].nama == name) {
+                        cout << "password " << name << ": ";
+                        getline(cin, pw);
+                        if (pw == user[i].password) {
+                            return name;
+                        }
+                    }
+                }
+            }
+            else {
+                cout << "Tidak dapat menemukan username, silahkan coba ulang atau periksa typo\n";
+                return "Tamu";
+            }
+
+            return "Tamu";
+        }
+        void update() {
+            ifstream data("datauser.txt");
+            if (data.is_open()) {
+                int index = 1;
+                string akses, nama, password, meminjam, judul;
+                while (getline(data, akses, ',')) {
+                    getline(data, nama, ',');
+                    getline(data, password, ',');
+                    getline(data, meminjam, ',');
+                    getline(data, judul);
+
+                    User user_iterasi;
+                    user_iterasi.index = index;
+                    user_iterasi.akses = akses;
+                    user_iterasi.nama = nama;
+                    user_iterasi.password = password;
+                    user_iterasi.meminjam = meminjam;
+                    user_iterasi.judul = judul;
+
+                    user.push_back(user_iterasi);
+                    index++;
+                }
+            data.close();
+            }
+
+            else {
+                cout << "Gagal membuka file User" << endl;
+            }
+        }
 };
 
 
 int main() {
     data_perpus buku;
-    int pil;
-    cin >> pil;
+    client pengguna;
 
-    if (pil == 1) {
-        buku.tampilkan_data();
-    }
-    else if (pil == 2) {
-        buku.tambah();
-    }
-    else if (pil == 3) {
-        buku.edit();
-    }
-    //buku.tambah();
-    //buku.tampilkan_data();
+    cout << "Perpusku CLI, silahkan login terlebih dahulu untuk melanjutkan\n"
+         << "Jika anda belum memiliki akun, masukkan 0 pada input nama\n"
+         << "ketik \\help untuk list perintah\n"
+         << "--------------------------------------------------------------\n\n";
+    //pengguna.login();
+    string prompt;
+    do {
+        cout <<"["<< user_sekarang << "] > ";
+        getline(cin, prompt);
+        if (prompt == "clear") {
+            system("clear");
+        }
+        else if (prompt == "login") {
+            user_sekarang = pengguna.login();
+        }
+        else if (prompt == "update") {
+            pengguna.update();
+        }
+
+    } while (prompt != "q");
+
+
 }
