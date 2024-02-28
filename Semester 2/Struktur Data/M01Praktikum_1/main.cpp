25e105f7274631c34b2873d15263d19dfc54a759
Hidayadie <hissats234@gmail.com>
2024-02-28 20:01:28 +0700

    initial commitan

---
+++ Semester 2/Struktur Data/M01Praktikum_1/main.cpp
@@ -0,0 +1,50 @@
+// Header sama namespace //
+#include <iostream>
+using namespace std;
+
+
+// Cetak biru //
+//void printArray();
+int minArray(int aray[], int *), maxArray(int aray[], int *);
+
+
+// Fungsi utama //
+int main() {
+    int ukuran;
+    cout << "Size array: "; cin >> ukuran;
+    int aray[ukuran];                       // Menggunakan ukuran input langsung tanpa menggunakan sizeof()/int()
+    for (int i = 0; i < ukuran; i++) {
+        cout << "Masukkan nilai array ke - " << i+1 << " : "; cin >> aray[i];
+    }
+    cout << "\n";
+    //system("clear");                      // Biar rapi
+    cout << "Nilai aray: ";
+    for (const auto& i : aray) {            // Memakai loop for each biar keren hehe
+        cout << i << " ";
+    }
+    cout << "\nNilai minimum: "<< minArray(aray, &ukuran)
+         << "\nNilai maximum: "<< maxArray(aray, &ukuran);
+}
+
+// Fungsi lain //
+
+int minArray(int aray[], int *ukuran) {
+    int kecil = aray[0];
+    for (int i = 0; i < *ukuran; i++) {
+        if (kecil > aray[i]) {
+            kecil = aray[i];
+        }
+    }
+    return kecil;
+}
+
+int maxArray(int aray[], int *ukuran) {
+    int besar = aray[0];
+    for (int i = 0; i < *ukuran; i++) {
+        if (besar < aray[i]) {
+            besar = aray[i];
+        }
+    }
+    return besar;
+}
+
