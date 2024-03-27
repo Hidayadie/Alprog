4dac5edcf76bcd5a490a7c1b448ea4a803ff7836
Adie <107916585+Hidayadie@users.noreply.github.com>
2024-03-27 15:41:32 +0700

    fgdfgregregreg

---
+++ Semester 2/Struktur Data/M05Praktikum_Queue/sources2.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <conio.h>
+#define Merah "\033[31m"
+#define Hitam "\033[0m"
+#define Hijau "\033[32m"
+#define Biru "\033[34m"
+
+using namespace std;
+
+const int Max = 3;
+
+struct Queue {
+	int depan, belakang, data[Max];
+};
+Queue queue;
+
+void init(), deque(), enque(), clear(), print();
+bool isEmpty(), isFull();
+
+int main () {
+	init();
+	int pil;
+	do {
+		system("cls");
+		cout << Hitam;
+		cout << ">>> PILIHAN MENU <<<\n"
+			 << "1. Tambah\n"
+			 << "2. Hapus\n"
+			 << "3. Print\n"
+			 << "4. Kosongkan\n"
+			 << "5. Keluar\n";
+		cout << "Sisa size: " << (queue.belakang + 1 == Max ? Merah : Hijau) << queue.belakang + 1 << "/" << Max <<"\n";
+		cout << (queue.belakang + 1 == Max ? Merah : Hijau) << (queue.belakang + 1 == Max ? "Queue Sudah Penuh!!!" : "Queue masih tersedia...");
+		cout << Hitam << "\n -> "; cin >> pil;
+		
+		switch (pil) {
+			case 1:
+				enque();
+				break;
+			case 2:
+				deque();
+				break;
+			case 3:
+				print();
+				break;
+			case 4:
+				clear();
+				break;
+			case 5:
+				continue;
+				break;
+			default:
+				cout << Merah << "\n\npilihan g ad... \n"<< Hitam <<"Enter untuk kembali";
+				getch();
+		}
+	} while (pil != 5); 
+}
+
+void init() {
+	queue.depan = queue.belakang = -1;
+}
+
+void enque() {
+	if (!isFull()) {
+		int data;
+		cout << "Data yang ingin ditambah = "; cin >> data;
+		queue.depan = 0;
+		queue.belakang++;
+		queue.data[queue.belakang] = data;
+		cout << "Data ok.\n\n";
+		
+	}
+	else {
+		cout << Merah << "Queue Penuh!\n" << Hitam;
+	}
+	cout << Biru << "Ketuk enter untuk kembali..." << Hitam;
+	getch();
+}
+
+void deque() {
+	cout << "Data "<< queue.data[queue.depan] << " dihapus\n\n";
+	for (int i = queue.depan; i <= queue.belakang; i++) {
+		queue.data[i] = queue.data[i + 1];
+	}
+	queue.belakang--;
+	cout << Biru << "Ketuk enter untuk kembali..." << Hitam;
+	getch();
+}
+
+void clear() {
+	queue.depan = queue.belakang = -1;
+	cout << "Kosong\n\n";
+	cout << Biru << "Ketuk enter untuk kembali..." << Hitam;
+	getch();
+}
+
+void print() {
+	cout << "isi: ";
+	for (int i = queue.depan; i <= queue.belakang; i++) {
+		cout << queue.data[i] << " ";
+	}
+	cout << "\n\n";
+	cout << Biru << "Ketuk enter untuk kembali..." << Hitam;
+	getch();
+}
+
+bool isEmpty() {
+	return (queue.belakang == -1);
+}
+
+bool isFull() {
+	return (queue.belakang >= (Max -1));
+}
