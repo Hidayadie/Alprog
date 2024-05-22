ff2d435f9553d49f6a463272928d61ae219b9000
Hidayadie <hissats234@gmail.com>
2024-05-22 19:48:49 +0700

    snyc

---
+++ Semester 2/Pemrograman/M03_Pengenalan_OOP/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace M03_Pengenalan_OOP {
+
+	Class Program {
+		static void main() {
+			Mahasiswa mahasiswa = new Mahasiswa();
+	
+			mahasiswa.Nama = "Jono";
+			mahasiswa.Nim = "12345";
+			mahasiswa.Ipk = 5;
+
+			mahasiswa.Registrasi();
+			mahasiswa.IsiKrs();		
+		}
+	}
+}
---
+++ Semester 2/Pemrograman/M04_Class_Member_1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Public void Main() {
+	console.WriteLine("tes");
+}
