753412d6360d0928e158dcc684040a6b7163e4cc
Adie <hidayadie@students.amikom.ac.id>
2024-06-04 10:13:42 +0700

    tambah minggu 10

---
+++ Semester 2/Pemrograman/M10Interface/M10Interface/Konversi.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+using System.IO;
+using System.Xml.Serialization;
+
+
+namespace M10Interface
+{
+    public class ConvertToJson : IConvertObject
+    {
+        public void Convert(Mahasiswa mhs)
+        {
+            string json = JsonConvert.SerializeObject(mhs);
+
+            Console.WriteLine("Hasil konversi: {json}");
+        }
+    }
+
+    public class ConvertToXml : IConvertObject
+    {
+        public void Convert(Mahasiswa mhs) 
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+
+                XmlSerializer serializer = new XmlSerializer(typeof(Mahasiswa));
+                serializer.Serialize(writer, mhs);
+
+                string xml = writer.ToString();
+
+                Console.WriteLine("Hasil xml: {xml}");
+             }
+        }
+    }
+
+    public class ConvertToCsv : IConvertObject
+    {
+        public void Convert(Mahasiswa mhs)
+        {
+            string csv = string.Format("{0},{1},{2}", mhs.npm, mhs.nama, mhs.email);
+            Console.WriteLine("Hasil konversi Csv: {csv}");
+        }
+    }
+}
---
+++ Semester 2/Pemrograman/M10Interface/M10Interface/Pelengkap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M10Interface
+{
+
+    public class Mahasiswa
+    {
+        public string npm;
+        public string nama;
+        public string email;
+        public Mahasiswa(string npm, string nama, string email) 
+        {
+            this.npm = npm;
+            this.nama = nama;
+            this.email = email;
+        }
+    }
+
+
+
+    public interface IConvertObject
+    {
+        void Convert(Mahasiswa mhs);
+    }
+}
