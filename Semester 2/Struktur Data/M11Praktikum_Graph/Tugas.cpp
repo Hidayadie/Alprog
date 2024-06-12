f5c0e3c7e07efb9bb22673260467be94c5a01549
Hidayadie <hidayadie@students.amikom.ac.id>
2024-06-12 16:45:20 +0700

    add

--- Semester 2/Struktur Data/M11Praktikum_Graph/Tugas.cpp
+++ Semester 2/Struktur Data/M11Praktikum_Graph/Tugas.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::cin;
@@ -20,11 +21,60 @@ int main() {
 		edge	,
 		sumber	,
 		tujuan	;
+	
+	
 		
+	bool kosong	,
+		 trigger;
+		 	
+		 	
 	cout << "Jumlah Vertex	= "; cin >> ukuran;
 	cout << "Jumlah Edge	= "; cin >> edge;
 	
-	int matrix[ukuran][ukuran] = {0};
+	cin.ignore();
 	
-		
+	// INISIALISASI GRAPH
+	int matrix[ukuran][ukuran] = {0};
+	char nama[ukuran];
+	
+	// FUNGSI PENAMAAN
+	
+	for (int i = 0; i < ukuran; i++) {
+		cout << "Nama Vertex ke - " << i+ 1 << " : "; cin >> nama[i];
+	}
+	
+	// FUNGSI ISI
+	
+	for (int i = 0; i < edge; i++) {
+		cout << "\nEdge " << i +1;
+		cout << "\nVertex Sumber	= "; cin >> sumber;
+		cout << "Vertex Tujuan 	= "; cin >> tujuan;
+		matrix[sumber-1][tujuan-1] = matrix[tujuan-1][sumber-1] = 1;
+	}
+
+
+	// FUNGSI CARI KEKOSONGAN
+
+	for (int i = 0; i < ukuran; i++) {
+		for (int j = 0; j < ukuran; j++) {
+			((matrix[i][j] == 1) ? trigger = true : trigger = false);
+			if (trigger) {
+				kosong = true;
+				break;
+			}
+		}
+	}
+	cout << "Graph Berhasil dibuat\n";
+	cout << ((!kosong) ? "Semua Graph Terhubung" : " ");
+	cout << "\n";
+	// FUNGSI CEK GARIS
+	
+	for (int i = 0; i < ukuran; i++) {
+		for (int j = 0; j < ukuran; j++) {
+			if (matrix[i][j] == 1) {
+				cout << nama[i] << nama[j] << ", ";
+			}
+		}
+	}
+	
 }
